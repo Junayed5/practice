3d97fa39c1800c77cc04daaf05fbfaf9f0211492
Junayed5 <mdjunayed352003@gmail.com>
2023-12-13 15:10:43 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,13 @@
+// find small to large number an array 
+const arr = [9,5,10,7,3,6,2];
+
+const number = arr.sort((a,b) => {
+    return a - b;
+});
+
+// find small to large number an array 
+const number2 = arr.sort((a,b) => {
+    return b - a;
+});
+
+// End this part
