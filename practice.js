cb2bbc7c370b3b8b7d797bd929247e7be054fd13
Junayed5 <mdjunayed352003@gmail.com>
2024-01-19 20:44:28 +0600

    find leap year

--- practice.js
+++ practice.js
@@ -120,4 +120,12 @@ const formSubmit = (event) => {
   
 };
 
+// How to find a leap year
 
+const getLeapYear = (year) => {
+  if (year % 400 === 0 || ((year % 4 ===0) && (year % 100 !== 0))) {
+    return `${year} is leap year`
+  } else{
+    return `${year} is not leap year`
+  }
+}
